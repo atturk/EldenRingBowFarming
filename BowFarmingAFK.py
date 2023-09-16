deacb525b2033801d5da3ef0392c9f0737984f40
Attilio <41196959+atturk@users.noreply.github.com>
2023-09-16 11:59:11 +0200

    Update BowFarmingAFK.py

--- BowFarmingAFK.py
+++ BowFarmingAFK.py
@@ -27,6 +27,20 @@ def sprint(dur):
     exit1()
     ReleaseKey(W)
     exit1()
+
+def sprintback(dur):
+    exit1()
+    PressKey(S)
+    exit1()
+    PressKey(SPACE)
+    exit1()
+    t.sleep(dur)
+    exit1()
+    ReleaseKey(SPACE)
+    exit1()
+    ReleaseKey(S)
+    exit1()
+
 def exit1():
     if k.is_pressed("enter"):
         raise KeyboardInterrupt
@@ -46,22 +60,39 @@ def main():
     while run:
         print(cycle)
         keyPress(G,0.1)
+        t.sleep(0.1)
         keyPress(F,0.1)
+        t.sleep(0.1)
         keyPress(F,0.1)
+        t.sleep(0.1)
         keyPress(E,0.1)
+        t.sleep(0.1)
         keyPress(E,0.1)
         t.sleep(5)
-        Mouse(-118*d1,24*d1,)
-        sprint(1)
+
+        #------
+
+        Mouse(-23*d,20*d)
+        sprint(3)
+        Mouse(-90*d,10*d)
+        sprint(0.3)
+        t.sleep(0.1)
+        Mouse(-20*d,-5*d)
+        t.sleep(0.5)
+        
+        #------
+        
         PressKey(J)
-        t.sleep(0.1)
-        PressKey(O)
-        t.sleep(0.1)
-        keyPress(P,0.1)
+        t.sleep(1)
+        keyPress(Q,0.1)
+        keyPress(P,0.5)
         t.sleep(5)
-        ReleaseKey(O)
+        keyPress(Q,0.1)
         ReleaseKey(J)
-        t.sleep(5)
+        t.sleep(0.5)
+        Mouse(160*d,-30*d)
+        keyPress(W,0.1)
+        sprint(7)
         cycle+=1
         
 
